009a4a99a14b76c0f8c6090cc80340edeb6f4af2
tgdivy <tgdivy@gmail.com>
2022-09-12 22:53:39 +0100

    hello world!

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
