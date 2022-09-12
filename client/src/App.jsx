a56a0c7587be93a282b339b15c87525c28c9f1e3
tgdivy <tgdivy@gmail.com>
2022-09-12 22:32:55 +0100

    init server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <div>
+        <input type="text" name="movieName" />
+        <input type="text" name="review" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
