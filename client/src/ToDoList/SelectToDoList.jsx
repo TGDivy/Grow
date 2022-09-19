dbc1995fd2f20e779cab7ef8f03d5296e0c9a263
tgdivy <tgdivy@gmail.com>
2022-09-19 20:38:37 +0100

    selectToDoList

---
+++ client/src/ToDoList/SelectToDoList.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
+import { Button, SwipeableDrawer } from "@mui/material";
+import {
+  LightModeOutlined,
+  FormatListBulletedOutlined,
+} from "@mui/icons-material";
+
+import propTypes from "prop-types";
+
+const SelectToDoList = ({ setList }) => {
+  const [open, setOpenState] = useState(false);
+
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setOpenState(open);
+  };
+
+  const Lists = () => {
+    const ListItems = [
+      { text: "Tasks 1", icon: <LightModeOutlined /> },
+      { text: "Tasks 2", icon: <FormatListBulletedOutlined /> },
+    ];
+    return (
+      <Box
+        sx={{ width: "auto" }}
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+      >
+        <List>
+          {ListItems.map((item) => (
+            <ListItem
+              disablePadding
+              key={item.text}
+              onClick={() => {
+                setList(item.text);
+              }}
+            >
+              <ListItemButton>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+  };
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>{"< Lists"}</Button>
+      <SwipeableDrawer
+        anchor={"top"}
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        disableDiscovery={iOS}
+      >
+        {Lists()}
+      </SwipeableDrawer>
+    </div>
+  );
+};
+
+SelectToDoList.propTypes = {
+  setList: propTypes.func.isRequired,
+};
+
+export default SelectToDoList;
--- client/src/ToDoList/ToDoList.jsx
+++ client/src/ToDoList/ToDoList.jsx
@@ -1,74 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Container, Box, Typography, Divider } from "@mui/material";
-import {
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListItemButton,
-} from "@mui/material";
-import { Button, SwipeableDrawer } from "@mui/material";
-import {
-  LightModeOutlined,
-  FormatListBulletedOutlined,
-} from "@mui/icons-material";
+import React, { useState } from "react";
+import { Container, Typography } from "@mui/material";
+import SelectToDoList from "./SelectToDoList";
 
 export const ToDoList = () => {
-  const [open, setOpenState] = useState(false);
-
-  const toggleDrawer = (open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setOpenState(open);
-  };
-
-  const list = () => (
-    <Box
-      sx={{ width: "auto" }}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <LightModeOutlined />
-            </ListItemIcon>
-            <ListItemText primary="My Day" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <FormatListBulletedOutlined />
-            </ListItemIcon>
-            <ListItemText primary="Tasks" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
-  );
+  const [list, setList] = useState("Tasks 1");
 
   return (
     <Container>
-      <Typography variant="h4">To Do List</Typography>
-      <div>
-        <Button onClick={toggleDrawer(true)}>{"top"}</Button>
-        <SwipeableDrawer
-          anchor={"top"}
-          open={open}
-          onClose={toggleDrawer(false)}
-          onOpen={toggleDrawer(true)}
-        >
-          {list()}
-        </SwipeableDrawer>
-      </div>
+      <SelectToDoList setList={setList} />
+      <Typography variant="h4">{list}</Typography>
     </Container>
   );
 };
