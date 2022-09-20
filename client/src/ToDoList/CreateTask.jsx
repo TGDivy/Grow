30c777cf86f383e809deeccde4e7c465c2c90c72
tgdivy <tgdivy@gmail.com>
2022-09-20 22:39:32 +0100

    dialog

--- client/src/ToDoList/CreateTask.jsx
+++ client/src/ToDoList/CreateTask.jsx
@@ -1,38 +1,32 @@
-import React from "react";
+import React, { forwardRef, useState } from "react";
 import propTypes from "prop-types";
 import { Card, CardHeader, Skeleton } from "@mui/material";
 import { IconButton } from "@mui/material";
-import {
-  CardContent,
-  Typography,
-  CardActions,
-  Collapse,
-  Chip,
-  Checkbox,
-  FormGroup,
-  FormControlLabel,
-} from "@mui/material";
-import {
-  MoreVert,
-  Favorite,
-  Share,
-  Expand,
-  Tag,
-  Add,
-} from "@mui/icons-material";
-import styled from "@emotion/styled";
+import { CardContent, Typography } from "@mui/material";
+import { Favorite, Add } from "@mui/icons-material";
 
-export const CreateTask = () => {
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const TaskSkeletonButton = ({ onClick }) => {
   return (
     <Card height="100%">
       <CardHeader
         title={<Typography variant="h5">Create Task</Typography>}
         action={
-          <IconButton aria-label="settings">
+          <IconButton onClick={onClick} aria-label="settings">
             <Add />
           </IconButton>
         }
-        subheader={[<Skeleton animation={false} key={1} />]}
       />
       <CardContent>
         <Skeleton animation={false} disable>
@@ -47,6 +41,43 @@ export const CreateTask = () => {
         </Skeleton>
       </CardContent>
     </Card>
+  );
+};
+
+TaskSkeletonButton.propTypes = {
+  onClick: propTypes.func.isRequired,
+};
+
+export const CreateTask = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <>
+      <TaskSkeletonButton onClick={handleClickOpen} />
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={() => setOpen(false)}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Disagree</Button>
+          <Button onClick={() => setOpen(false)}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
