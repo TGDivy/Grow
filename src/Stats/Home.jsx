840f156512ef76bf99583968eae5443c6464a255
tgdivy <tgdivy@gmail.com>
2022-10-14 02:38:13 +0100

    [FEAT ✨]: Tabs and ability to toggle dates!

--- src/Stats/Home.jsx
+++ src/Stats/Home.jsx
@@ -7,7 +7,11 @@ import { onSnapshot, doc } from "firebase/firestore";
 
 import useTimerRecordsStore from "../Common/Stores/TimerRecordsStore";
 
+import { Tab, Tabs, Button, IconButton } from "@mui/material";
+import { ArrowForward, ArrowBack } from "@mui/icons-material";
+
 import WeeklyWorkStat from "./WeeklyWorkStat";
+import WorkStatLine from "./WorkStatLine";
 import TagPieStat from "./TagPieStat";
 import TagRadarStat from "./TagRadarStat";
 
@@ -17,8 +21,8 @@ const filterTimerRecords = (timerRecords, timePeriodLength, daysBack) => {
   const current = new Date(Date.now() - daysBack * DAY);
   const filteredTimerRecords = timerRecords.filter(
     (timerRecord) =>
-      timerRecord.startTime.getTime() >=
-        current.getTime() - timePeriodLength * DAY &&
+      timerRecord.startTime.getTime() >
+        current.getTime() - (timePeriodLength - 1) * DAY &&
       timerRecord.startTime.getTime() < current.getTime()
   );
   // Pad weeklyTimerRecords with empty days
@@ -44,17 +48,24 @@ const Home = () => {
   );
   const timerRecords = useTimerRecordsStore((state) => state.timerRecords);
 
-  const daysBack = 0;
-  const timePeriodLength = 7;
+  // const daysBack = -0;
+  const [periodBack, setPeriodBack] = React.useState(0);
+  const [timePeriodLength, setTimePeriodLength] = React.useState(7);
+
+  const handleChange = (event, newValue) => {
+    setPeriodBack(0);
+    setTimePeriodLength(newValue);
+  };
+
   const selectedPeriod = filterTimerRecords(
     timerRecords,
     timePeriodLength,
-    daysBack
+    periodBack * timePeriodLength
   );
   const previousPeriod = filterTimerRecords(
     timerRecords,
     timePeriodLength,
-    daysBack + timePeriodLength
+    periodBack * timePeriodLength + timePeriodLength
   );
 
   useEffect(() => {
@@ -71,6 +82,31 @@ const Home = () => {
     return unsub;
   }, []);
 
+  // Display selected period, and change daysBack to change the period
+  const handlePeriodBack = (direction) => {
+    if (direction === "forward" && periodBack > 0) {
+      setPeriodBack(periodBack - 1);
+    } else if (direction === "backward") {
+      setPeriodBack(periodBack + 1);
+    }
+  };
+
+  const displayPeriod = () => {
+    const DAY = 24 * 60 * 60 * 1000;
+    const current = new Date(Date.now() - periodBack * DAY * timePeriodLength);
+
+    // Display current date in a pretty way
+    return (
+      <Typography variant="h6">
+        {current.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+      </Typography>
+    );
+  };
+
   return (
     <>
       <Container>
@@ -82,7 +118,52 @@ const Home = () => {
           }}
         />
 
-        <Grid container spacing={2} alignContent="center">
+        <Grid
+          container
+          spacing={2}
+          alignContent="center"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={8}>
+            <Tabs
+              value={timePeriodLength}
+              onChange={handleChange}
+              textColor="primary"
+              indicatorColor="primary"
+            >
+              <Tab value={3} label="3 Day" />
+              <Tab value={7} label="Week" />
+              <Tab value={14} label="Fortnight" />
+              <Tab value={30} label="Month" />
+            </Tabs>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Grid container spacing={2} alignContent="center">
+              <Grid item xs={3}>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={() => handlePeriodBack("backward")}
+                >
+                  <ArrowBack />
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                {displayPeriod()}
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={() => handlePeriodBack("forward")}
+                  disabled={periodBack === 0}
+                >
+                  <ArrowForward />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
           <Grid item xs={6} md={6}>
             <Typography variant="h6">Total Work Time</Typography>
             <Typography variant="h4">
@@ -106,6 +187,13 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} md={6}>
             <TagPieStat timerRecords={selectedPeriod} />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <WorkStatLine
+              timerRecords={selectedPeriod}
+              previousRecords={previousPeriod}
+              dayDiff={timePeriodLength}
+            />
           </Grid>
         </Grid>
       </Container>
