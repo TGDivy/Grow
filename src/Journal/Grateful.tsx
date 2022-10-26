0374beab104ab432f684c09199a442d9f9288816
tgdivy <tgdivy@gmail.com>
2022-10-26 01:16:08 +0100

    [FEAT ✨]: RTE in box!

--- src/Journal/Grateful.tsx
+++ src/Journal/Grateful.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useEffect } from "react";
 import useJournalStore from "../Common/Stores/DailyJournalStore";
 import RTE from "./RTE/RTE";
 
-const ReflectGrateful = () => {
+const Reflect = () => {
   // A place to write down what you're grateful for.
   const grateful = useJournalStore((state) => state.grateful);
   const setGrateful = useJournalStore((state) => state.setGrateful);
 
-  return <RTE text={grateful} setText={setGrateful} />;
+  return (
+    <>
+      <Box
+        sx={{
+          position: "relative",
+          height: "500px",
+          width: "100%",
+          "& .editor-inner": {
+            height: "450px",
+          },
+        }}
+      >
+        <RTE text={grateful} setText={setGrateful} textToAdd="Good Job!" />
+      </Box>
+    </>
+  );
 };
 
-export default ReflectGrateful;
+export default Reflect;
