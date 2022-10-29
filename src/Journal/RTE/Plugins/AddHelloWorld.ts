9262d6e3abbaeb5a74f2f7ff05628c11b0979c87
tgdivy <me@divyb.xyz>
2022-10-29 22:41:39 +0100

    [FEAT ✨]: Add new line when inserting the heading if not empty!

--- src/Journal/RTE/Plugins/AddHelloWorld.ts
+++ src/Journal/RTE/Plugins/AddHelloWorld.ts
@@ -1,6 +1,6 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useEffect } from "react";
-import { $createTextNode } from "lexical";
+import { $createParagraphNode, $createTextNode } from "lexical";
 import { $getRoot } from "lexical";
 import { $createHeadingNode } from "@lexical/rich-text";
 
@@ -20,12 +20,23 @@ export default function AddHelloWorldPlugin({ textToAdd }: props) {
           const headingNode = $createHeadingNode("h1");
           const textNode = $createTextNode(textToAdd);
           textNode.setMode("token");
+          textNode.setStyle("color: #ac9172");
           headingNode.append(textNode);
 
+          if (text !== "") {
+            // Add a new paragraph node
+            const paragraphNode = $createParagraphNode();
+            paragraphNode.append(headingNode);
+            root.append(paragraphNode);
+          }
+
           const lastChild = root.getLastChild();
+
           if (lastChild?.getTextContent() === "") {
             lastChild.insertBefore(headingNode);
           } else {
+            // lastChild?.insertBefore(lastChild);
+            // lastChild?.replaceWith($createHeadingNode("h1"));
             lastChild?.insertAfter(headingNode);
           }
         }
