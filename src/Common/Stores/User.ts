21c03f2376e5c5b57620c246dbcaa6ecea303f25
tgdivy <tgdivy@gmail.com>
2022-11-11 13:12:33 +0000

    [FEAT ✨]: Push tags when changed to firestore!

--- src/Common/Stores/User.ts
+++ src/Common/Stores/User.ts
@@ -2,10 +2,20 @@ import create from "zustand";
 import { devtools } from "zustand/middleware";
 import { userType } from "../Types/Types";
 
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../Firestore/firebase-config";
+
 interface userStoreType extends userType {
   setUser: (user: userType) => void;
   setTags: (tags: string[]) => void;
 }
+
+const updateTags = async (user_id: string, tags: string[]) => {
+  const userDocRef = doc(db, "users", user_id);
+  await updateDoc(userDocRef, {
+    tags: tags,
+  });
+};
 
 const useUserStore = create<userStoreType>()(
   devtools(
@@ -17,7 +27,10 @@ const useUserStore = create<userStoreType>()(
       photoURL: "",
       tags: [],
       setUser: (user) => set({ ...user }),
-      setTags: (tags) => set({ tags }),
+      setTags: (tags) => {
+        set({ tags: tags });
+        updateTags(get().uid, tags);
+      },
     }),
     {
       name: "user-storage",
