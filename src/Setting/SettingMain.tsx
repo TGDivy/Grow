807fc8a7130681ae1f74f10c0ac6d219d79910a7
tgdivy <tgdivy@gmail.com>
2022-11-11 13:45:34 +0000

    [FEAT ✨]: Custom tags settings page and set!

--- src/Setting/SettingMain.tsx
+++ src/Setting/SettingMain.tsx
@@ -1,7 +1,126 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Paper,
+  Grid,
+  Typography,
+  Button,
+  Divider,
+} from "@mui/material";
+import { TextField, Chip } from "@mui/material";
+import { Add } from "@mui/icons-material";
+
+import useUserStore from "../Common/Stores/User";
+import { Stack } from "@mui/system";
 
 const SettingMain = () => {
-  return <div>SettingMain</div>;
+  // Text box for adding new tags
+  // And display all the tags
+
+  const tags = useUserStore((state) => state.tags);
+  const setTags = useUserStore((state) => state.setTags);
+
+  const [tag, setTag] = React.useState("");
+
+  const handleAddTag = () => {
+    // tags are unique, case insensitive and not empty
+    if (
+      tag.trim() !== "" &&
+      !tags.map((t) => t.toLowerCase()).includes(tag.toLowerCase())
+    ) {
+      setTags([...tags, tag]);
+    }
+  };
+
+  const handleDeleteTag = (tag: string) => {
+    setTags(tags.filter((t) => t !== tag));
+  };
+
+  return (
+    <Container>
+      <Stack spacing={2}>
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h3" align="center">
+            Settings
+          </Typography>
+        </Box>
+        <Divider />
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "space-between",
+            height: "100%",
+            backgroundColor: "#ffffff88",
+            color: "black",
+          }}
+        >
+          <Grid
+            container
+            spacing={1}
+            sx={{ width: "100%" }}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant="h4"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                Tags
+              </Typography>
+            </Grid>
+
+            <Grid item xs={4}>
+              <TextField
+                id="outlined-basic"
+                label="Add new tag"
+                variant="outlined"
+                size="small"
+                value={tag}
+                onChange={(event) => {
+                  setTag(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={handleAddTag}>
+                <Add />
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Box sx={{ mt: 2, width: "100%" }}>
+            <Grid
+              container
+              spacing={1}
+              // keep the tags left aligned
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              {tags.map((tag) => (
+                <Grid item key={tag}>
+                  <Chip
+                    label={tag}
+                    onDelete={() => handleDeleteTag(tag)}
+                    sx={{
+                      textTransform: "capitalize",
+                    }}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Paper>
+      </Stack>
+    </Container>
+  );
 };
 
 export default SettingMain;
