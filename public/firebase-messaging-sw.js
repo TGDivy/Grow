e35442842cb95f69bdc2ac5714fd12f572e38417
tgdivy <divy.work@outlook.com>
2023-03-05 17:08:34 +0000

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -20,3 +20,19 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+// Customize notification handler
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
